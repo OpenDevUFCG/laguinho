6af3cbdc6f9cbcdd8281c20f37bb24fd515fcc85
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2021-06-03 13:16:01 -0300

    eslint added

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier/react', 'prettier/javascript'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
