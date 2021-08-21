9ad6a1d7406d523aca1a74b73855b0d86c5777ca
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2021-08-21 01:10:03 -0300

    banner and navigation menu added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,33 @@
+import Link from 'next/link';
 import styles from '@styles/components/Header.module.css';
+import banner from '@assets/laguinho/banner.png';
 
 function Header() {
-  return <header className={styles.container}>Header</header>;
+  return (
+    <header className={styles.container}>
+      <div className={styles.navigation}>
+        <Link href="/">Início</Link>
+        <Link href="/">Sobre</Link>
+        <Link href="/">Datasets</Link>
+        <Link passHref href="/">
+          OpenDevUFCG
+        </Link>
+      </div>
+
+      <div className={styles.menu}>
+        <img src={banner} alt="Banner do Laguinho" />
+
+        <div>
+          <input type="text" />
+        </div>
+
+        <div>
+          <button type="button">Registrar</button>
+          <button type="button">Login</button>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
