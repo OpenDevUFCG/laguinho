c8916fa593490b476ac384d824e2bdd3d7b21411
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2021-08-21 00:26:53 -0300

    header component added

---
+++ src/components/Header/index.js
@@ -0,0 +1,7 @@
+import styles from '@styles/components/Header.module.css';
+
+function Header() {
+  return <header className={styles.container}>Header</header>;
+}
+
+export default Header;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,15 @@
 /* eslint-disable react/forbid-prop-types */
 import PropTypes from 'prop-types';
+import Header from '../components/Header';
 import '@styles/globals.css';
 
 function CustomApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 CustomApp.propTypes = {
